ae2fd29fcaea240428f0cf535c8974f64587e48a
StaPi420 <kuramsinravil9@gmail.com>
2025-10-09 10:50:50 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            int a = int.Parse(Console.ReadLine());
+            int[] ans = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (array[i, j] > a)
+                        ++ans[i];
+                }
+            }
+            foreach(int elem in ans)
+                Console.Write($"{elem} ");
+        }
+    }
+}
