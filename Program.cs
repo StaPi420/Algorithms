a3158cbf920b0f5400492854b0adb35f0f7ff37c
StaPi420 <kuramsinravil9@gmail.com>
2025-10-22 20:55:01 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{   
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        String str = Console.ReadLine();
+
+        Console.Write("Введите подстроку: ");
+        String substr = Console.ReadLine();
+
+        int res = str.Split(substr).Length - 1;
+
+        Console.WriteLine(res);
+    }
+}
