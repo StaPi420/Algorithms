148c44b39b808de899f0b703e5f02cb6f31643a2
StaPi420 <kuramsinravil9@gmail.com>
2025-11-13 11:28:39 +0400

    Update pr14_tsk1_var9.cs

--- pr14_tsk1_var9.cs
+++ pr14_tsk1_var9.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text;
 using System.IO;
 using System.Collections.Generic;
 
@@ -7,57 +6,53 @@ class Program
 {
     static void Main()
     {
-        using (StreamReader reader = new StreamReader("C:\\Users\\kuram\\.vscode\\csproj\\fileIn.txt"))
+        using (StreamReader reader = new StreamReader("C:\\Users\\contest\\fileIn3.txt"))
         {
             int n = int.Parse(reader.ReadLine());
             SPoint[] pointAr = new SPoint[n];
+            int[] amount = new int[n];
             for (int i = 0; i < n; ++i)
             {
                 string[] line = reader.ReadLine().Split(' ');
                 //pointAr[i] = new SPoint();
                 pointAr[i].x = int.Parse(line[0]);
                 pointAr[i].y = int.Parse(line[1]);
-                pointAr[i].z = int.Parse(line[2]);
             }
             int r = int.Parse(Console.ReadLine());
-            int ansIndx = 0, maxAmount = 0;
+            int maxAmount = 0;
             for (int i = 0; i < n; ++i)
             {
-                int curAmount = 0;
                 for (int j = 0; j < n; ++j)
                 {
                     if (pointAr[i].getDistance(pointAr[j]) <= r)
                     {
-                        ++curAmount;
+                        ++amount[i];
                     }
                 }
-                Console.WriteLine(curAmount);
-                if (curAmount > maxAmount)
+                Console.WriteLine(amount[i]);
+                maxAmount = Math.Max(maxAmount, amount[i]);
+            }
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\contest\\fileOut.txt"))
+            {
+                for (int i = 0; i < n; ++i)
                 {
-                    maxAmount = curAmount;
-                    ansIndx = i;
+                    if (amount[i] == maxAmount)
+                    {
+                        writer.WriteLine($"{pointAr[i].x} {pointAr[i].y}");
+                    }
                 }
             }
-            Console.WriteLine($"{pointAr[ansIndx].x} {pointAr[ansIndx].y} {pointAr[ansIndx].z}");
         }
     }
 }
 
 struct SPoint
 {
-    public int x, y, z;
-    public SPoint(int x, int y, int z)
-    {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
+    public int x, y;
     public int getDistance(SPoint other)
     {
         int xOrt = Math.Abs(this.x * this.x - other.x * other.x);
         int yOrt = Math.Abs(this.y * this.y - other.y * other.y);
-        int zOrt = Math.Abs(this.z * this.z - other.z * other.z);
-        return Math.Abs(xOrt + yOrt + zOrt);
+        return Math.Abs(xOrt + yOrt);
     }
 }
