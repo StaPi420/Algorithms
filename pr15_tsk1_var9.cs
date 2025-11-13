213a30231bf86352824179eb29f4a50e6eb603f9
StaPi420 <kuramsinravil9@gmail.com>
2025-11-13 10:32:20 +0400

    Update pr15_tsk1_var9.cs

--- pr15_tsk1_var9.cs
+++ pr15_tsk1_var9.cs
@@ -1,13 +1,12 @@
 using System;
-using System.Text;
 using System.IO;
-using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        using (StreamReader reader = new StreamReader("C:\\Users\\kuram\\.vscode\\csproj\\fileIn.txt"))
+        using (StreamReader reader = new StreamReader("C:\\Users\\contest\\fileIn1.txt"))
         {
             int n = int.Parse(reader.ReadLine());
             string[] ar = new string[n];
@@ -15,15 +14,14 @@ class Program
             {
                 ar[i] = reader.ReadLine();
             }
-            var ansArray = from number in ar
+            var ansArray = from number in ar 
                            where number.Length == 2
                            select int.Parse(number) + 5;
-            
-            foreach (int number  in ansArray)
+
+            foreach (int number in ansArray)
             {
                 Console.Write($"{number} ");
             }
         }
     }
 }
-
