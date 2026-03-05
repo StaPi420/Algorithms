da6e0a5f72ec16f42ec50985c6d2b0c772f95b5f
StaPi420 <kuramsinravil9@gmail.com>
2026-03-05 10:31:52 +0400

    Update task1_var9.cs

--- pr21/task1_var9.cs
+++ pr21/task1_var9.cs
@@ -25,15 +25,43 @@ namespace ConsoleApp1
         {
             head = null;
         }
-        public void Add(int valueInf)
+        private void Add(int valueInf, ref Node r)
+        {
+            if (r == null)
+            {
+                r = new Node(valueInf);
+            }
+            else
+            {
+                if (r.inf > valueInf)
+                {
+                    Add(valueInf, ref r.left);
+                }
+                else
+                {
+                    Add(valueInf, ref r.right);
+                }
+            }
+        }
+        public void Push(int valueInf)
         {
             if (head == null)
             {
-                Node r = new Node(valueInf);
-                head = r;
-                return;
+                head = new Node(valueInf);
             }
-
+            else
+            {
+                Add(valueInf, ref head);
+            }
+        }
+        public int FindMin()
+        {
+            Node r = head;
+            while (r.left != null)
+            {
+                r = r.left;
+            }
+            return r.inf;
         }
     }
     class Program
@@ -41,7 +69,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             char[] sep = { ' ', ',', '.', '!', '?', '\n', '\t' };
-            MyQueue queue = new MyQueue();
+            BinaryTree tree = new BinaryTree();
             using (StreamReader reader = new StreamReader("C:\\Users\\kuramshinrr\\Desktop\\input1.txt"))
             {
                 using (StreamWriter writer = new StreamWriter("C:\\Users\\kuramshinrr\\Desktop\\output.txt"))
@@ -51,14 +79,14 @@ namespace ConsoleApp1
                     {
                         int num = int.Parse(s);
                         writer.Write($"{num} ");
-                        queue.Push(num);
+                        tree.Push(num);
                     }
                     writer.WriteLine();
-                    queue.RemoveAllMax();
-                    queue.Print(writer);
+                    int min = tree.FindMin();
+                    writer.Write(min);
                 }
             }
-            queue = new MyQueue();
+            tree = new BinaryTree();
             using (StreamReader reader = new StreamReader("C:\\Users\\kuramshinrr\\Desktop\\input2.txt"))
             {
                 using (StreamWriter writer = new StreamWriter("C:\\Users\\kuramshinrr\\Desktop\\output2.txt"))
@@ -68,11 +96,11 @@ namespace ConsoleApp1
                     {
                         int num = int.Parse(s);
                         writer.Write($"{num} ");
-                        queue.Push(num);
+                        tree.Push(num);
                     }
                     writer.WriteLine();
-                    queue.RemoveAllMax();
-                    queue.Print(writer);
+                    int min = tree.FindMin();
+                    writer.Write(min);
                 }
             }
         }
