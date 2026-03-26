c67244e3ed02d080c3dd0639bf32ef66f9330c7f
StaPi420 <kuramsinravil9@gmail.com>
2026-03-26 11:14:54 +0400

    Add files via upload

---
+++ pr18-19/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Car : Vehicle
+    {
+        public override void Show()
+        {
+            Console.WriteLine($"Марка: {brand}\nНомер: {carNumber}\nСкорость: {speed}\nГрузоподъёмность:{liftingCapacity}");
+        }
+        public override int LiftingCapacity
+        {
+            get
+            {
+                return liftingCapacity;
+            }
+        }
+    }
+}
---
+++ pr18-19/Motorcycle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Motorcycle : Vehicle
+    {
+        private bool hasSidecar = false;
+        public Motorcycle(String brand, String carNumber, int speed)
+        {
+            this.brand = brand;
+            this.carNumber = carNumber;
+            this.speed = speed;
+            this.liftingCapacity = 0;
+        }
+        public Motorcycle(String brand, String carNumber, int speed, int liftingCapacity) : base(brand, carNumber, speed, liftingCapacity)
+        {
+            hasSidecar = true;
+        }
+        public override int LiftingCapacity
+        {
+            get
+            {
+                return LiftingCapacity;
+            }
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Марка: {brand}\nНомер: {carNumber}\nСкорость: {speed}\nГрузоподъёмность: {liftingCapacity}\nЕсть коляска{hasSidecars}");
+        }
+    }
+}
