7514f14d08a24c233128c9bfb39ea2cd0c840fe1
StaPi420 <kuramsinravil9@gmail.com>
2026-04-30 10:52:16 +0400

    Update Program.cs

--- pr18-19/Program.cs
+++ pr18-19/Program.cs
@@ -1,24 +1,24 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Immutable;
 using System.ComponentModel;
+using System.Text.Json;
 using ConsoleApp2;
 
 class Program
 {
     private static Random random = new Random();
-    public static string GetRandomNumber()
+    private static string filePath = "data.json";
+
+    public static Vehicle[] GetAll()
     {
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        return new string(Enumerable.Repeat(chars, 5)
-            .Select(s => s[random.Next(s.Length)]).ToArray());
+        if (!File.Exists(filePath))
+            return new Vehicle[100];
+
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<Vehicle[]>(json) ?? new();
     }
-    public static string GetRandomBrand()
-    {
-        string[] brands = new string[] {"BMW", "LADA", "FORD", "MERSEDEZ"};
-        return brands[random.Next(4)];
-    }
-    public static void Main()
+    public static void GenerateFile()
     {
         Vehicle[] vehicles = new Vehicle[100];
         for (int i = 0; i < 34; ++i)
@@ -33,6 +33,33 @@ class Program
         {
             vehicles[i] = new Truck(GetRandomBrand(), GetRandomNumber(), random.Next(70, 150), random.Next(0, 10000), random.Next(2) == 0);
         }
+        Save(vehicles);
+    }
+
+    public static void Save(Vehicle[] faculties)
+    {
+        var json = JsonSerializer.Serialize(faculties, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
+        File.WriteAllText(filePath, json);
+    }
+    public static string GetRandomNumber()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, 5)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+    public static string GetRandomBrand()
+    {
+        string[] brands = new string[] { "BMW", "LADA", "FORD", "MERSEDEZ" };
+        return brands[random.Next(4)];
+    }
+    public static void Main()
+    {
+        GenerateFile();
+        var vehicles = GetAll();
         Console.Write("Введите минимальную необходимую грузоподьёмность:");
         int minLiftingCapacity = int.Parse(Console.ReadLine());
         var satysfyingVehicles = vehicles
