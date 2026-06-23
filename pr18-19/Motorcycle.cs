cdd304cdc6db10c4a128c7447d453509478f6396
StaPi420 <kuramsinravil9@gmail.com>
2026-06-23 22:16:37 +0400

    Update Motorcycle.cs

--- pr18-19/Motorcycle.cs
+++ pr18-19/Motorcycle.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Runtime.Serialization;
 namespace ConsoleApp2
 {
+    [DataContract]
     class Motorcycle : Vehicle
     {
+        [DataMember]
         private bool hasSidecar = false;
         public Motorcycle(String brand, String carNumber, int speed, int liftingCapacity, bool hasSidecar) : base(brand, carNumber, speed, liftingCapacity)
         {
@@ -19,6 +21,10 @@ namespace ConsoleApp2
             {
                 return hasSidecar ? liftingCapacity : 0;
             }
+        }
+        public override string ToString()
+        {
+            return $"Motorcycle\nBrand: {brand}\nCar Number: {carNumber}\nSpeed: {speed}\nLifting Capacity: {liftingCapacity}\nHas Sidecar: {hasSidecar}";
         }
     }
 }
