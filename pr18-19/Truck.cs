1f586ee77e3c9cfe5afe8ad62f528ad7ea7d34fd
StaPi420 <kuramsinravil9@gmail.com>
2026-06-23 22:16:06 +0400

    Update Truck.cs

--- pr18-19/Truck.cs
+++ pr18-19/Truck.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace ConsoleApp2
 {
+    [DataContract]
     public class Truck : Vehicle
     {
+        [DataMember]
         private bool hasTrailer = false;
         public Truck(String brand, String carNumber, int speed, int liftingCapacity, bool hasTrailer) : base(brand, carNumber, speed, liftingCapacity)
         {
@@ -31,6 +34,10 @@ namespace ConsoleApp2
             {
                 hasTrailer = value;
             }
+        }
+        public override string ToString()
+        {
+            return $"Truck\nBrand: {brand}\nCar Number: {carNumber}\nSpeed: {speed}\nLifting Capacity: {liftingCapacity}\nHas Trailer: {hasTrailer}";
         }
     }
 }
